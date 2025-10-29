69a86b0fc08f8768525d5ffb4a0d387b0d5732fc
kasia Magryta <107499943+kasiula55555@users.noreply.github.com>
2025-10-29 12:51:33 +0100

    Dodano funkcje analizy cech hasla w features.py

---
+++ src/ai_psa/features.py
@@ -0,0 +1,6 @@
+cammon_pass = {
+    "123456","password","123456789","qwerty","abc123","111111","123123","letmein",
+    "admin","welcome","iloveyou","monkey","dragon","sunshine","princess"
+}
+
+sequences = ["abcdefghijklmnopqrstuvwxyz", "qwertyuiop", "asdfghjkl", "zxcvbnm", "0123456789"]
