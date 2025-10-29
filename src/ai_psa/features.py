4a23e4aacc7e2b6c01accafd272fbcfa8418e67b
kasia Magryta <107499943+kasiula55555@users.noreply.github.com>
2025-10-29 13:17:31 +0100

    hasSeq

--- src/ai_psa/features.py
+++ src/ai_psa/features.py
@@ -13,3 +13,19 @@ def shannon_entropy(passwd: str) -> float:
     n = len(passwd)
 
     return -sum((c/n) * math.log2(c/n) for c in counts.values())
+
+
+def has_sequence(passwd:str, min_run:int = 3) -> bool:
+    passwd_lower = passwd.lower()
+
+    for seq in sequences:
+        for i in range(len(seq) - min_run -1):
+            run = seq[i:i+min_run]
+            if run in passwd_lower or run[::-1] in passwd_lower:
+                return True
+        rseq = seq[::-1]
+        for i in range(len(rseq) - min_run -1):
+            run = rseq[i:i+min_run]
+            if run in passwd_lower:
+                return True
+    return False
